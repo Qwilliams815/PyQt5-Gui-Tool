3ad25de180d3026de29fbae12320ffe64d5d4542
Brian Williams <brianman815@gmail.com>
2021-03-27 09:51:40 -0700

    Completed: Added hitting enter adds tab functionality Added empty tab name validator Added tab closing functionality
    Working on:
    Independant tab profiles
    Individual list memory per profile.

--- tool_main.py
+++ tool_main.py
@@ -1,12 +1,17 @@
 import sys
 import os
+# import uuid
 from tool import Ui_MainWindow
 from PyQt5 import QtWidgets as qtw
 from PyQt5 import QtCore as qtc
 from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtCore import QSettings
 
 apps = set()
+profiles = set()
 
+# CREATING A SAVE FILE FOR THE ADDED APPS
 if not os.path.isfile('save.txt'):
     mem = open('save.txt', 'w+')
 
@@ -27,6 +32,7 @@ class tool_window(qtw.QMainWindow, Ui_MainWindow):
         #self.ui = Ui_MainWindow()
         #self.ui.setupUi(self)
         self.setupUi(self)
+        #self.setStyleSheet("background-color: grey;")
 
         #lOADS SAVED PREVIOUSLY ADDED APPS
         with open('save.txt', 'r') as f:
@@ -35,44 +41,101 @@ class tool_window(qtw.QMainWindow, Ui_MainWindow):
             for app in tempApps:
                 apps.add(app)
                 self.seperate_listWidget.addItem(app)
-
+        
+        # PROFILE/TABS
         self.proButton.clicked.connect(self.add_tab)
+        self.proNameEdit.returnPressed.connect(self.add_tab)
 
+        self.tabWidget.setTabsClosable(True)
+        self.tabWidget.setMovable(True)
+
+        # Emits the index of which tab is requesting closure
+        self.tabWidget.tabCloseRequested.connect(self.remove_tab)
+        
+
+        # APPLICATIONS/LISTS
         self.appButton.clicked.connect(self.add_app)
         
-        self.launchButton.clicked.connect(self.launch)
+        # LAUNCH BUTTON
+        #self.launchButton.clicked.connect(self.launch)
+        self.launchButton.clicked.connect(self.test)
         
 
 
 
-       #self.proButton.clicked.connect(self.add_profile)
+        #self.proButton.clicked.connect(self.add_profile)
         #self.new_tab.setObjectName("new_tab")
         #self.new_tab = qtw.QWidget()
 
     # Put all Functions(Methods) Here:
 
     def add_tab(self):
-        self.new_tab = qtw.QWidget()
-        #self.new_tab.setObjectName("new_tab")
         proName = self.proNameEdit.text()
-        self.tabWidget.addTab(self.new_tab, f"{proName}")
-        self.proNameEdit.clear()
+        profiles.add(proName)
+        print(profiles)
+        #tab_id = proName + str(uuid.uuid4())
+        #print(tab_id)
+
+        if proName != "": # EMPTY TAB NAME VALIDATER
+            self.new_tab = qtw.QWidget()
+            #self.new_tab.setObjectName("new_tab")
+            self.tabWidget.addTab(self.new_tab, f"{proName}")
+            self.proNameEdit.clear()
+
+            #CREATES LISTWIDGET WITHIN TAB
+            self.newTab_listWidget = qtw.QListWidget(self.new_tab)
+            self.newTab_listWidget.setGeometry(qtc.QRect(20, 10, 281, 441))
+            #self.newTab_listWidget.setObjectName(f"{proName}_listWidget")
+            #print(self.f"{proName}_listWidget")
+
+        else: # EMPTY TAB NAME ERROR POP-UP MSG
+            msg = QMessageBox()
+            msg.setWindowTitle("Error")
+            msg.setText("Error: Profile name cannot be blank.")
+            msg.setIcon(QMessageBox.Warning)
+            x = msg.exec_()
+
+
+    def remove_tab(self, index):
+        # print("close_handler called, index = %s" % index)
+        self.tabWidget.removeTab(index)
 
     def add_app(self):
 
         #if os.path.isfile('save.txt'):
         
-                
-
+        # Returns tuple: (filename, file type specifications)
         fileName = QFileDialog.getOpenFileName(self, 'Open file', 'c:\\', 'executables (*.exe)')
+
+        #for profile in profiles:
+        #    if profile == self.tabWidget.tabText(self.tabWidget.currentIndex()):
+        #        pass
 
         # DUPLICATE FILE QUALIFIER
         if fileName[0] not in apps:
+
             apps.add(fileName[0])
-            self.seperate_listWidget.addItem(fileName[0])
-            with open('save.txt', 'w') as f:
-                for application in apps:
-                    f.write(application + ',')
+            #current_tab = self.tabWidget.tabText(self.tabWidget.currentIndex())
+
+            # self.seperate_listWidget.addItem(fileName[0])
+            for profile in profiles:
+                pass
+
+            self.tab1_listWidget.addItem(fileName[0])
+            #$self.currentListWidget = self.tabWidget.tabText(self.tabWidget.currentIndex()) + "_listWidget"
+            #$self.currentListWidget.addItem(fileName[0])
+
+
+            # SAVING THE APP
+            #with open('save.txt', 'w') as f:
+            #    for application in apps:
+            #        f.write(application + ',')
+
+        #self.tabWidget.currentWidget.tab.tab1_listWidget.addItem(fileName[0])
+
+
+
+
         print(apps)
 
     def launch(self):
@@ -80,12 +143,19 @@ class tool_window(qtw.QMainWindow, Ui_MainWindow):
             os.startfile(app)
 
 
+    def test(self):
+        #print(self.tabWidget.tabText(self.tabWidget.currentIndex()))
+        #print(self.tabWidget.currentIndex())
+        #print(self.tabWidget.tabText(self.tabWidget.currentIndex()) + "_listWidget")
+        #print(self.tabWidget.currentWidget.tab1_listWidget)
 
-        
-
-
-    def test_message(self):
-        print("Hello there")
+        #print([tab for tab in range(0, self.tabWidget.count()) if self.tabWidget.currentIndex() == tab])
+        for tab in range(0, self.tabWidget.count()):
+            if self.tabWidget.currentIndex() == tab:
+                self.newTab_listWidget.addItem("Hello")
+                print(self.tabWidget.currentIndex())
+        #        self.tabWidget.count[tab]
+        #print(self.findChildren(self.tabWidget))
 
 
 if __name__ == '__main__':
